b54ad120cab074ee5050c7df6ed3b9b49054b1de
이승수 <>
2020-12-15 18:04:50 +0900

    2차적인틀

--- server.py
+++ server.py
@@ -45,19 +45,67 @@ def Login():
 
     return render_template('login.html')
 
-@app.route('/home/<int:sID>')
+@app.route('/home/<int:sID>', methods=['GET','POST'])
 def home(sID):
     db = sqlite3.connect("todo_list.db")
     db.row_factory = sqlite3.Row
-    # items = db.execute(
-    #     'select cName from Sugang, Class where Sugang.cID=Class.cID and sID=?',(sID,)
-    # ).fetchall()
-    items = db.execute(
-        'select VAHName, VideoAndHomework.cID from VideoAndHomework where VideoAndHomework.cID in (select Sugang.cID from Sugang, Class where Sugang.cID=Class.cID and sID=?)',(sID,)
-    )
-    #db.close()
-    return render_template('home.html', items= items)
+    
+    if request.method == "POST":
+        db2 = sqlite3.connect("todo_list.db")
+        c = db2.cursor()
+        db2.row_factory = lambda cursor, row: row[0]
 
+
+        VAHs = db2.execute(
+        'SELECT VAHName FROM VideoAndHomework, Class WHERE sID=? and VideoAndHomework.cID = Class.cID',(sID,)
+        ).fetchall()
+        Decisions = db2.execute(
+            'SELECT Decision FROM VideoAndHomework, Class WHERE sID=? and VideoAndHomework.cID = Class.cID',(sID,)
+        ).fetchall()
+        count = 0
+        for VAH in VAHs:
+            
+            #print(Decisions)
+            if request.form.get(VAH):
+                if Decisions[count] == 'Y':
+
+                    c.execute(
+                        """UPDATE VideoAndHomework set Decision='N' where VAHName=?""",(VAH,)
+                    )
+                    db2.commit()
+                    print(count)
+                    print("Y로 변경")
+                elif Decisions[count] == 'N':
+
+                    c.execute(
+                        """UPDATE VideoAndHomework set Decision='Y' where VAHName=?""",(VAH,)
+                    )
+                    db2.commit()
+                    print(count)
+                    print("N으로 변경")
+                #print(VAH)
+            count = count + 1
+        
+        # Decisions = db.execute(
+        #     'SELECT Decision FROM VideoAndHomework, Class WHERE sID=? and VideoAndHomework.cID = Class.cID',(sID,)
+        # ).fetchall()
+        # print(Decisions[0][0])
+        db2.close()
+        # submit = request.form["학기프로젝트"]
+        # print(VAH)
+        # print(submit)
+        
+        # db.execute(
+        #     'UPDATE VideoAndHomework SET dicision = "Y" WHERE sID'
+        # )
+
+    items = db.execute(
+        'SELECT VAHName, Class.cID,cName,Decision,DeadLine FROM VideoAndHomework, Class WHERE sID=? and VideoAndHomework.cID = Class.cID',(sID,)
+    ).fetchall()
+
+    
+    db.close()
+    return render_template('home.html', items= items)
 
 
 if __name__ == '__main__':
